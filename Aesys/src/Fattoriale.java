1c43787b9263f46bcf4638fd1857e11c907a4569
tos97 <47986842+tos97@users.noreply.github.com>
2021-05-06 09:38:53 +0200

    Esercizio 06/05/2021 con miglioramento del fattoriale con il tipo biginteger fatto

--- Aesys/src/Fattoriale.java
+++ Aesys/src/Fattoriale.java
@@ -1,21 +1,29 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Fattoriale {
+
     public static void Gestore() {
         Scanner sc = new Scanner(System.in);
         int k;
+        BigInteger s = BigInteger.ONE;
         System.out.println("Inserisci un numero:");
         k = sc.nextInt();
         System.out.print("\n" + k + "! = ");
-        Stampa(k);
+        Stampa(k, s);
+
     }
 
-    public static void Stampa(int n){
-        if(n == 1 || n == 0)
-            System.out.println("1\n");
+    public static void Stampa(int n, BigInteger s){
+        if(n < 0)
+            System.out.println("ERRORE\nN negativo\n");
         else {
-            System.out.print(n + " * ");
-            Stampa(n-1);
+            if (n == 1 || n == 0)
+                System.out.println(s +"\n");
+            else {
+                s = s.multiply(BigInteger.valueOf(n));
+                Stampa(n - 1, s);
+            }
         }
     }
 }
